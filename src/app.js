f31259d9496889989ec4bce5179b29f644c2b407
Mfuamba <Munekayi.dev@gmail.com>
2024-12-19 09:30:20 +0200

    Backend, CORS fixed

--- src/app.js
+++ src/app.js
@@ -1,26 +1,23 @@
 const express = require('express');
-
+const path = require('path');
 const cors = require('cors');
 const compression = require('compression');
-
 const cookieParser = require('cookie-parser');
-
 const coreAuthRouter = require('./routes/coreRoutes/coreAuth');
 const coreApiRouter = require('./routes/coreRoutes/coreApi');
 const coreDownloadRouter = require('./routes/coreRoutes/coreDownloadRouter');
 const corePublicRouter = require('./routes/coreRoutes/corePublicRouter');
 const adminAuth = require('./controllers/coreControllers/adminAuth');
-
 const errorHandlers = require('./handlers/errorHandlers');
 const erpApiRouter = require('./routes/appRoutes/appApi');
 
-const fileUpload = require('express-fileupload');
 // create our Express app
 const app = express();
 
+// Use CORS and other middlewares
 app.use(
   cors({
-    origin: ['http://localhost:3000','http://localhost:8888','https://thembi.onrender.com/','https://thembi.kizuri.co.za/','https://thembi.onrender.com','https://thembi.kizuri.co.za'], // Adjust to your frontend's origin
+    origin: ['http://localhost:3000', 'http://localhost:8888', 'https://thembi.onrender.com', 'https://thembi.kizuri.co.za'],
     credentials: true,
     allowedHeaders: ['Authorization', 'Content-Type'],
   })
@@ -29,25 +26,30 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(compression());
 
-// // default options
-// app.use(fileUpload());
+// Serve static files from the dist folder
+app.use(express.static(path.join(__dirname, 'dist')));
 
-// Here our API Routes
-
+// API Routes
 app.use('/api', coreAuthRouter);
 app.use('/api', adminAuth.isValidAuthToken, coreApiRouter);
 app.use('/api', adminAuth.isValidAuthToken, erpApiRouter);
 app.use('/download', coreDownloadRouter);
 app.use('/public', corePublicRouter);
 
-// If that above routes didnt work, we 404 them and forward to error handler
-app.use(errorHandlers.notFound);
+// Redirect all other routes to the index.html (for React Router handling)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
-// production error handler
+// Error handling
+app.use(errorHandlers.notFound);
 app.use(errorHandlers.productionErrors);
 
-// done! we export it so we can start the site in start.js
+// Start the server
+const server = app.listen(process.env.PORT || 8888, () => {
+  console.log(`Express server running on port ${server.address().port}`);
+});
+
 module.exports = app;
